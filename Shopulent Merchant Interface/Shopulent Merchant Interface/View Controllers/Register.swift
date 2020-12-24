76134e6aa9e6d7e44c800f9e34fea95faacb687d
Rheeders <sparky79131>
2020-12-24 09:40:08 +0200

    Completed

--- Shopulent Merchant Interface/Shopulent Merchant Interface/View Controllers/Register.swift
+++ Shopulent Merchant Interface/Shopulent Merchant Interface/View Controllers/Register.swift
@@ -6,6 +6,7 @@
 //
 
 import Cocoa
+import FirebaseAuth
 
 class Register: NSViewController {
     
@@ -28,6 +29,19 @@ class Register: NSViewController {
         let email: String = emailTextField.stringValue
         let confirmPassword: String = confirmTextField.stringValue
         if (Utilities.isValidEmail(email: email) && Utilities.isValidPassword(password: password) && Utilities.isPasswordsMatch(password: password, confirmPassword: confirmPassword)){
+            
+            Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                if let error = error {
+                    let alert = NSAlert()
+                    alert.messageText = "Error While Creating Account"
+                    alert.informativeText = error.localizedDescription
+                    alert.addButton(withTitle: "OK")
+                    alert.alertStyle = NSAlert.Style.critical
+                    alert.runModal()
+                } else {
+                    
+                }
+            }
             
             }
     }
